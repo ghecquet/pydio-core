4314f2033d4bbbc8ca7d702b0cecc52cffd46d2a
Greg Hecquet <ghecquet@groupe-comelli.com>
2015-10-06 15:53:25 +0200

    Revert "Review of the pull request 830 and refactoring"
    This reverts commit 8ff00a2d9c908e845dbb08810c50a0c8730fdd3e.

--- core/src/plugins/action.powerfs/class.PowerFSController.php
+++ core/src/plugins/action.powerfs/class.PowerFSController.php
@@ -47,8 +47,8 @@ class PowerFSController extends AJXP_Plugin
         }
         $urlBase = "ajxp.fs://". ConfService::getRepository()->getId();
         $mess = ConfService::getMessages();
-		
         switch ($action) {
+
             case "monitor_compression" :
 
                 $percentFile = fsAccessWrapper::getRealFSReference($urlBase.$dir."/.zip_operation_".$httpVars["ope_id"]);
@@ -98,6 +98,7 @@ class PowerFSController extends AJXP_Plugin
                 break;
 
             case "postcompress_download":
+
                 $archive = AJXP_Utils::getAjxpTmpDir().DIRECTORY_SEPARATOR.$httpVars["ope_id"]."_".AJXP_Utils::sanitize(AJXP_Utils::decodeSecureMagic($httpVars["archive_name"]), AJXP_SANITIZE_FILENAME);
                 $fsDriver = AJXP_PluginsService::getInstance()->getUniqueActivePluginForType("access");
                 if (is_file($archive)) {
@@ -112,7 +113,6 @@ class PowerFSController extends AJXP_Plugin
             case "precompress" :
 
                 $archiveName = AJXP_Utils::sanitize(AJXP_Utils::decodeSecureMagic($httpVars["archive_name"]), AJXP_SANITIZE_FILENAME);
-				
                 if (!ConfService::currentContextIsCommandLine() && ConfService::backgroundActionsSupported()) {
                     $opeId = substr(md5(time()),0,10);
                     $httpVars["ope_id"] = $opeId;
@@ -124,7 +124,6 @@ class PowerFSController extends AJXP_Plugin
                         "on_end" => (isSet($httpVars["on_end"])?$httpVars["on_end"]:"reload"),
                         "ope_id" => $opeId
                     );
-					
                     AJXP_XMLWriter::triggerBgAction(
                         "monitor_compression",
                         $bgParameters,
@@ -134,304 +133,81 @@ class PowerFSController extends AJXP_Plugin
                     session_write_close();
                     exit();
                 }
-				
-				$rootDir = fsAccessWrapper::getRealFSReference($urlBase . $dir);
-				
-				// List all files
-				$args = array();
-				foreach ($selection->getFiles() as $selectionFile) {
-					$baseFile = $selectionFile;
-					$args[] = escapeshellarg(substr($selectionFile, strlen($dir)+($dir=="/"?0:1)));
-					$selectionFile = fsAccessWrapper::getRealFSReference($urlBase.$selectionFile);
-					
-					if(trim($baseFile, "/") == ""){
-						// ROOT IS SELECTED, FIX IT
-						$args = array(escapeshellarg(basename($rootDir)));
-						$rootDir = dirname($rootDir);
-						break;
-					}
-				}
-				
-				$files = "";
-				foreach ($args as &$value)
-				{
-					$valuef = $rootDir."\\".str_replace("\"","",$value);
-					if (is_dir($valuef))
-						$files = $files . $this->read_dir_recursive($valuef, $rootDir); 
-					else if (substr($value, 0, 5) != ".axp.")
-						$files = $files . str_replace("\"","",$value) . "\r\n"; 
-				}
-				$files = substr($files, 0, strlen($files) - 2);
-				
-				$compressLocally = ($action == "compress" ? true : false);
-				$baseDir = "";
-				$baseName = $archiveName;
-				
-				// Setting parameters
-				$zipper = $this->_getZipperOptions();
-				
-				if (!$compressLocally) {
-					$baseDir = AJXP_Utils::getAjxpTmpDir();
-					$baseName = $httpVars["ope_id"] . "_" . $archiveName;
-				}
-				
-				// Building files
-				$outputFilesPath = $this->_buildCompressionFiles(
-					$rootDir,
-					$baseName,
-					$httpVars["ope_id"],
-					$files
-				);
-				
-				// Building command
-				$command = $this->_buildCompressionCommand(
-					$rootDir,
-					$baseDir . $baseName,
-					$files,
-					$zipper,
-					$outputFilesPath
-				);
-				
-				// Triggering command
-				AJXP_Logger::debug(__CLASS__,__FUNCTION__,"Executing zip command");
-				AJXP_Logger::debug(__CLASS__,__FUNCTION__,$command);
-				$proc = popen($command, 'r');
-		
-				// Monitoring
-				$this->_buildMonitoring($proc, $zipper, $outputFilesPath);
 
-				// Closing
-				AJXP_Logger::debug(__CLASS__,__FUNCTION__,"Closing Proc");
-				pclose($proc);
+                $rootDir = fsAccessWrapper::getRealFSReference($urlBase) . $dir;
+                $percentFile = $rootDir."/.zip_operation_".$httpVars["ope_id"];
+                $compressLocally = ($action == "compress" ? true : false);
+                // List all files
+                $todo = array();
+                $args = array();
+                $replaceSearch = array($rootDir, "\\");
+                $replaceReplace = array("", "/");
+                foreach ($selection->getFiles() as $selectionFile) {
+                    $baseFile = $selectionFile;
+                    $args[] = escapeshellarg(substr($selectionFile, strlen($dir)+($dir=="/"?0:1)));
+                    $selectionFile = fsAccessWrapper::getRealFSReference($urlBase.$selectionFile);
+                    $todo[] = ltrim(str_replace($replaceSearch, $replaceReplace, $selectionFile), "/");
+                    if (is_dir($selectionFile)) {
+                        $objects = new RecursiveIteratorIterator(new RecursiveDirectoryIterator($selectionFile), RecursiveIteratorIterator::SELF_FIRST);
+                        foreach ($objects as $name => $object) {
+                            $todo[] = str_replace($replaceSearch, $replaceReplace, $name);
+                        }
+                    }
+                    if(trim($baseFile, "/") == ""){
+                        // ROOT IS SELECTED, FIX IT
+                        $args = array(escapeshellarg(basename($rootDir)));
+                        $rootDir = dirname($rootDir);
+                        break;
+                    }
+                }
+                $cmdSeparator = ((PHP_OS == "WIN32" || PHP_OS == "WINNT" || PHP_OS == "Windows")? "&" : ";");
+                if (!$compressLocally) {
+                    $archiveName = AJXP_Utils::getAjxpTmpDir().DIRECTORY_SEPARATOR.$httpVars["ope_id"]."_".$archiveName;
+                }
+                chdir($rootDir);
+                $cmd = $this->getFilteredOption("ZIP_PATH")." -r ".escapeshellarg($archiveName)." ".implode(" ", $args);
+                $fsDriver = AJXP_PluginsService::getInstance()->getUniqueActivePluginForType("access");
+                $c = $fsDriver->getConfigs();
+                if ((!isSet($c["SHOW_HIDDEN_FILES"]) || $c["SHOW_HIDDEN_FILES"] == false) && stripos(PHP_OS, "win") === false) {
+                    $cmd .= " -x .\*";
+                }
+                $cmd .= " ".$cmdSeparator." echo ZIP_FINISHED";
+                $proc = popen($cmd, "r");
+                $toks = array();
+                $handled = array();
+                $finishedEchoed = false;
+                while (!feof($proc)) {
+                    set_time_limit (20);
+                    $results = fgets($proc, 256);
+                    if (strlen($results) == 0) {
+                    } else {
+                        $tok = strtok($results, "\n");
+                        while ($tok !== false) {
+                            $toks[] = $tok;
+                            if ($tok == "ZIP_FINISHED") {
+                                $finishedEchoed = true;
+                            } else {
+                                $test = preg_match('/(\w+): (.*) \(([^\(]+)\) \(([^\(]+)\)/', $tok, $matches);
+                                if ($test !== false) {
+                                    $handled[] = $matches[2];
+                                }
+                            }
+                            $tok = strtok("\n");
+                        }
+                        if($finishedEchoed) $percent = 100;
+                        else $percent = min( round(count($handled) / count($todo) * 100),  100);
+                        file_put_contents($percentFile, $percent);
+                    }
+                    // avoid a busy wait
+                    if($percent < 100) usleep(1);
+                }
+                pclose($proc);
+                file_put_contents($percentFile, 100);
 
-				// Clearing
-				$this->_clear($outputFilesPath);
-				
                 break;
             default:
                 break;
         }
+
     }
-	
-	/*
-	 * Setting ZIP Parameters Options
-	 */
-	private function _getZipperOptions() {
-		
-		$zip_type = $this->getFilteredOption("ZIP_TYPE");
-		$zip_path = $this->getFilteredOption("ZIP_PATH");
-
-		if($this->getFilteredOption("MAX_ZIP_TIME") != 0)
-            $max_zip_time = intval($this->getFilteredOption("MAX_ZIP_TIME"));
-        else
-            $max_zip_time = 120;
-			
-		// TODO - TO CHECK
-		//if (substr($zip_path, -1) != "\\" || substr($zip_path, -1) != "/") $zip_path = $zip_path . "\\";
-			
-		$ret = array(
-			"type" => $zip_type,
-			"path" => $zip_path,
-			"max_zip_time" =>  $max_zip_time
-		);
-		
-		return $ret;
-	}
-	
-	/*
-	 * Building the different Files needed by the tools
-	 */
-	private function _buildCompressionFiles($rootDir, $baseName, $ope_id, $files) {
-		
-		$filesname     = $rootDir ."/.axp.".$baseName.".files.txt";
-		$logfile       = $rootDir ."/.axp.".$baseName.".processed.txt";
-		$clioutputfile = $rootDir ."/.axp.".$baseName.".cli_output.log";
-		$clierrorfile  = $rootDir ."/.axp.".$baseName.".cli_error.log";
-		$zipspeedfile  = $rootDir ."/.axp.".$baseName.".avgspeed.txt";
-		$percentfile   = $rootDir ."/.zip_operation_".$ope_id;
-		
-		file_put_contents($filesname, $files);
-		
-		$ret = array (
-			'filesname'     => $filesname,
-			'logfile'       => $logfile,
-			'clioutputfile' => $clioutputfile,
-			'clierrorfile'  => $clierrorfile,
-			'zipspeedfile'  => $zipspeedfile,
-			'percentfile'   => $percentfile
-		);
-		
-		//AJXP_Logger::debug(__CLASS__,__FUNCTION__,"Files :\r\n" . $ret);
-		return $ret;
-		
-	}
-	
-	/*
-	 * Building the command that is going to be used to restore the file
-	 */
-	private function _buildCompressionCommand($rootDir, $archiveName, $files, $zipper, $outputFilesPath)
-	{
-		$zip_type = $zipper["type"];
-		$zip_path = $zipper["path"];
-		
-		$filesname = $outputFilesPath["filesname"];
-		
-		//Setting file names
-		$cmdSeparator = ((PHP_OS == "WIN32" || PHP_OS == "WINNT" || PHP_OS == "Windows")? "&" : ";");
-		
-		//Changing root dir
-		chdir($rootDir);
-		AJXP_Logger::debug(__CLASS__,__FUNCTION__, "Current Dir : " . getcwd());
-
-		switch ($zip_type){
-			case "zip" :
-				$cmd = $zip_path . " -r ".escapeshellarg($archiveName)." ". "\"" . str_replace("\r\n", "\" \"", $files) . "\"";
-				break;
-			case "winrar" :
-				$cmd = $zip_path . " a -afzip " . escapeshellarg($archiveName) . " @" . escapeshellarg($filesname);
-				break;
-			case "7zip" :
-				$cmd = $zip_path . " a -tzip ".escapeshellarg($archiveName)." @" . escapeshellarg($filesname);
-				break;
-			case "other" :
-				$zip_exe = $zip_path;
-				$zip_exe = str_replace("%archive%", escapeshellarg($archiveName), $zip_exe);
-				$zip_exe = str_replace("%files%", "\"" . implode("\" \"", $args) . "\"" , $zip_exe);
-				$zip_exe = str_replace("%listfile%", escapeshellarg($filesname), $zip_exe);
-				$zip_exe = str_replace("%outputfile%", escapeshellarg($logfile), $zip_exe);
-				
-				$cmd = $zip_path . $zip_exe;
-				break;
-			default:
-				throw new AJXP_Exception("Wrong zip tool");
-				break;
-		}
-		
-		if (stripos(PHP_OS, "win") === false) {
-			$fsDriver = AJXP_PluginsService::getInstance()->getUniqueActivePluginForType("access");
-			$c = $fsDriver->getConfigs();
-			if (!isSet($c["SHOW_HIDDEN_FILES"]) || $c["SHOW_HIDDEN_FILES"] == false) {
-				$cmd .= " -x .\*";
-			}
-		}
-		
-		$cmd .= " ".$cmdSeparator." echo ZIP_FINISHED";
-
-		return $cmd;
-	}
-	
-	/*
-	 * Building the monitoring system. Looking through the output file to see if we have our exit command
-	 */
-	private function _buildMonitoring($proc, $zipper, $outputFilesPath) {
-		
-		$zip_type = $zipper["type"];
-		$max_zip_time = $zipper["max_zip_time"];
-		
-		$percentFile = $outputFilesPath['percentfile'];
-		
-		//
-		$toks = array();
-		$handled = array();
-		$finishedEchoed = false;
-		
-		$percent = 0;
-		
-		//select & configure monitoring process
-		switch ($zip_type) {
-			case "winrar" :
-				$recogpattern = "/%file%/";
-				break;
-			default :
-				$recogpattern = '/(\w+): (%file%) \(([^\(]+)\) \(([^\(]+)\)/';
-				break;
-		}
-		
-		// Monitoring progress
-		if (is_resource($proc)) {
-			while (!feof($proc)) {
-				set_time_limit ($max_zip_time);
-				$results = fgets($proc, 256);
-			
-				if (strlen($results) == 0) {
-				
-				} else {
-					$tok = strtok($results, "\n");
-			
-					while ($tok !== false) {
-						$toks[] = $tok;
-						if ($tok == "ZIP_FINISHED") {
-							$finishedEchoed = true;
-						} else {
-							$test = preg_match('/(\w+): (.*) \(([^\(]+)\) \(([^\(]+)\)/', $tok, $matches);
-							if ($test !== false) {
-								$handled[] = $matches[2];
-							}
-						}
-						$tok = strtok("\n");
-					}
-					
-					if($finishedEchoed) $percent = 100;
-					else $percent = min( round(count($handled) / count($todo) * 100),  100);
-					file_put_contents($percentFile, $percent);
-				}
-				
-				// avoid a busy wait
-				if($percent < 100) usleep(1);
-			}
-			
-			if(! $finishedEchoed) throw new AJXP_Exception("Could not zip");
-		} else {
-			AJXP_Logger::logAction(__CLASS__,__FUNCTION__, "Zip Process could not be started.");
-			file_put_contents($percentFile, 100);
-			
-			throw new AJXP_Exception("Could not start zip process");
-		}
-		
-		file_put_contents($percentFile, 100);
-	}
-	
-	/*
-	 * Clear the files and release file handles if necessary
-	 */
-	private function _clear($outputFilePaths) {
-		$filesname = $outputFilePaths['filesname'];
-		$logfile = $outputFilePaths['logfile'];
-		$clioutputfile = $outputFilePaths['clioutputfile'];
-		$clierrorfile = $outputFilePaths['clierrorfile'];
-		$zipspeedfile = $outputFilePaths['zipspeedfile'];
-		
-		if (file_exists($filesname)) unlink($filesname);
-		if (file_exists($logfile)) unlink($logfile);
-		if (file_exists($clioutputfile)) unlink($clioutputfile);
-		if (file_exists($clierrorfile)) {
-			$fcontent = file_get_contents($clierrorfile);
-			unlink($clierrorfile);
-			if (strlen($fcontent) > 2) AJXP_Logger::logAction(__CLASS__,__FUNCTION__,"Command Line Error " . $fcontent);
-		}
-		if (file_exists($zipspeedfile)) unlink($zipspeedfile);
-	}
-	
-	// Helper function to retrieve file names
-	public function read_dir_recursive($dir, $rootdir) 
-	{ 
-	    $handle =  opendir($dir);
-		$filesr = "";
-
-	    while ($arg = readdir($handle)) 
-	    { 
-			$val = $dir."\\".$arg;
-	        if ($arg != "." && $arg != "..") 
-	        { 
-	            if (is_dir($val))
-	                $filesr = $filesr . $this->read_dir_recursive($val, $rootdir); 
-	            else if (strpos($val, ".axp.") === false)
-	                $filesr = $filesr . substr($val, strlen($rootdir) + 1) . "\r\n"; 
-	        }
-	    }
-		
-	    closedir($handle);
-		
-		return $filesr;
-	} 
 }
